f1b99a67c5cd7876fa10b2b2d4331c58acc4a330
santoshi <santusubedi96@gmail.com>
2017-03-17 12:30:12 +0545

    This is basic datatypes

---
+++ basictypes.py
@@ -0,0 +1,9 @@
+an_integer = 6
+a_floating_point = 17.68
+a_boolean = True
+a_string = "Foo"
+
+print "Integer value =", an_integer
+print "Float value =", a_floating_point
+print "Boolean value=", a_boolean
+print "String value =", a_string
