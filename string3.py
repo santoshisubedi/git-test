da5d64f28f7a67852281ea7a73eb7daa56f2d2bf
santoshi <santusubedi96@gmail.com>
2017-03-18 09:56:13 +0545

    This is me

---
+++ string3.py
@@ -0,0 +1,10 @@
+first_name = raw_input('your first name:')
+last_name = raw_input('your last name:')
+
+print 'Hello %s %s !' % (first_name, last_name)
+
+print 'Hello {} {}!' . format(first_name, last_name)
+print 'Hello {0} {1}!' . format(first_name, last_name)
+
+print 'Hello {1} {0}!' . format(first_name, last_name)
+print 'Hello {1} {0} {1}!' . format(first_name, last_name)
