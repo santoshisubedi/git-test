73270b3360f4629286f45833ae47129214de2daa
santoshi <santusubedi96@gmail.com>
2017-03-18 09:47:47 +0545

    This is radius of circle

---
+++ string2.py
@@ -0,0 +1,5 @@
+#ask the user to enter first name and last name
+PI = 3.1415
+radius = raw_input("enter radius of the circle(meters)")
+area = PI * float(radius) **2
+print "\nmeters of circle = % .2f sq.meters" % area
