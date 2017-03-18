51a9dcb55edcb701c75c2bc3cbaaf0fb3215f3f7
santoshi <santusubedi96@gmail.com>
2017-03-18 08:57:45 +0545

    This is string

---
+++ string.py
@@ -0,0 +1,5 @@
+s = raw_input ('enter a string: ')
+print 'you have entered ' + s
+print ' no of charecter = %d' %len(s) 
+print ' first character = %s' %s[0]
+print 'last character = %s' %s[len(s) -1]
