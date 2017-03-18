17d4e5c4bbc949e2e396a80272875fac589e0f7a
santoshi <santusubedi96@gmail.com>
2017-03-18 09:40:00 +0545

    This is my name

---
+++ string1.py
@@ -0,0 +1,6 @@
+#ask the user to enter first and last name.
+first_name = raw_input('your first name :')
+last_name = raw_input('your last name:')
+
+print "\nHi %s %s!" % (first_name, last_name)
+print "It's nice to meet you."
