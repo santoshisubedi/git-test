a8a0940c05836ad8a0b1a7036a24fb04bb734057
santoshi <santusubedi96@gmail.com>
2017-03-24 10:54:18 +0545

    this is range of age

---
+++ program4.py
@@ -0,0 +1,31 @@
+age = int(input('enter your age\n'))
+if age <=0:
+	print("invalid input for age.")
+elif age <=1:
+	print("you are an infant.")
+elif age <=12:
+	print("you're just a kid.")
+elif age <=19:
+	print("you're a teenager.")
+elif age <=45:
+	print("you are adult now.")
+elif age <=59:
+	print("you're middle-aged.")
+elif age <=120:
+	print("you're old.")
+else:
+	print("you're too old to alive.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
