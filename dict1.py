8a93f954039e5e07ecb91ca3d828d54f675d91a6
santoshi <santusubedi96@gmail.com>
2017-03-25 09:31:34 +0545

    this is the loop over dictionaries

--- dict1.py
+++ dict1.py
@@ -2,7 +2,7 @@ user = {
     'name': 'santu subedi',
     'email': 'santusub@example.com',
     'phone': '9860144308',
-    'address': '123 6th St. Melbourne, FL 32904',
+    'address': 'ghattekulo,kathmandu',
 }
 
 for (key, val) in user.items():
